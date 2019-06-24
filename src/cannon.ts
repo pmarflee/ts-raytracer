af2fdac1ca42add4a052f8c4582d073527ea63d7
Paul Marfleet <paul.marfleet@gmail.com>
2019-06-24 19:39:04 +0100

    Implemented cannon simulation (page 13)

---
+++ src/cannon.ts
@@ -0,0 +1,27 @@
+import { Tuple, Tuples } from './tuples';
+
+export class Projectile {
+    constructor(readonly position: Tuple, readonly velocity: Tuple) {
+    }
+}
+
+export class Environment {
+    constructor(readonly gravity: Tuple, readonly wind: Tuple) {
+    }
+}
+
+function tick(environment: Environment, projectile: Projectile) : Projectile {
+    let position = Tuples.add(projectile.position, projectile.velocity),
+        velocity = Tuples.add(Tuples.add(projectile.velocity, environment.gravity), environment.wind);
+
+    return new Projectile(position, velocity);
+}
+
+let projectile = new Projectile(Tuples.point(0, 1, 0), Tuples.normalize(Tuples.vector(1, 1, 0))),
+    environment = new Environment(Tuples.vector(0, -0.1, 0), Tuples.vector(-0.01, 0, 0)),
+    i = 0;
+
+while (projectile.position[1] > 0) {
+    projectile = tick(environment, projectile);
+    console.log(`position: ${ projectile.position }, ticks: ${ ++i }`);
+}
